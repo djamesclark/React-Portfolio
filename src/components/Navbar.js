f9da2c0b100ba082ea4246fe0a49135edbb253c0
djamesclark <djamesclark@gmail.com>
2023-06-22 10:13:42 -0400

    updated readme

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ export default function NavBar({page, setPage}) {
                         <Nav.Link href="#aboutme" onClick={()=> setPage('about')}>About Me</Nav.Link>
                         <Nav.Link href="#portfolio" onClick={()=> setPage('portfolio')}>Portfolio</Nav.Link>
                         <Nav.Link href="#contact" onClick={()=> setPage('contact')}>Contact</Nav.Link>
-                        <Nav.Link href="#resume">Resume</Nav.Link>
+                        <Nav.Link href="https://docs.google.com/document/d/1N1EJ9PQpNZlJvxwNYH2ix3AYEseuJ_CLZLTZ7fAKtt8/edit">Resume</Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
